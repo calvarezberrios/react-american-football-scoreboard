30c5e2d52b3041cac80f0e49f5936aef4d8e2a94
Mannie <carlos.alvarezberrios@gmail.com>
2020-04-08 00:35:27 -0400

    Moved files to organize components and assets onto respective folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
-import "./App.css";
-import BottomRow from "./BottomRow";
+import "./assets/css/App.css";
+import BottomRow from "./components/BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
